378acb68a6593ee63c032691caf77ca36589050e
Goku <18100664+Vikas-V2@users.noreply.github.com>
2024-05-17 12:51:40 +0530

    update links

---
+++ vendorsetup.sh
@@ -0,0 +1,11 @@
+git clone -b 14.0 https://github.com/pixel-crdroid/proprietary_vendor_google_cheetah.git vendor/google/cheetah
+git clone -b 14.0 https://github.com/pixel-crdroid/vendor_google_panther.git vendor/google/panther
+git clone -b 14.0 https://github.com/pixel-crdroid/proprietary_vendor_google_lynx.git vendor/google/lynx
+git clone -b 14.0 https://github.com/pixel-crdroid/android_device_google_gs101.git  device/google/gs101
+git clone -b 14.0 https://github.com/pixel-crdroid/android_device_google_gs201.git  device/google/gs201
+git clone -b 14.0 https://github.com/pixel-crdroid/android_device_google_gs-common.git  device/google/gs-common
+git clone -b udc https://github.com/pixel-crdroid/PixelParts.git  packages/apps/PixelParts
+git clone -b 14.0 https://github.com/pixel-crdroid/android_device_google_pantah.git device/google/pantah
+git clone https://android.googlesource.com/device/google/lynx-kernel device/google/lynx-kernel 
+git clone https://android.googlesource.com/device/google/pantah-kernel device/google/pantah-kernel
+#git clone git@gitea.com:Vikas-V2/pixel_vendor_gapps.git vendor/gms
